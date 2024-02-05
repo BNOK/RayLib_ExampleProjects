af07f484343cc280b06966f39fbea1983435301d
BNOK <52835202+BNOK@users.noreply.github.com>
2024-02-05 16:15:25 +0100

    Update Classes and create Tick

--- Top_DownSection/main.cpp
+++ Top_DownSection/main.cpp
@@ -5,89 +5,94 @@
 
 // --------------- Window Struct ---------------
 const Vector2 Win_Settings{960, 640};
-class Character;
-class Map;
+
+class Character{
+private:
+    Vector2 ScreenPos{
+        (float)Win_Settings.x / 2.0f - 4.0f * (0.5f * (float)this->Current.width / 6.0f),
+        (float)Win_Settings.y / 2.0f - 4.0f * (0.5f * (float)this->Current.height)
+    };
+    Vector2 WorldPos{0.0f,0.0f};
+
+    // -------- Animation Settings -------------
+    int Frame{};
+    int MaxFrame{};
+    float RunningTime{};
+    float UpdateTime{};
+    float Flip{};
+
+public:
+    Texture2D Idle;
+    Texture2D Run;
+    Texture2D Current;
+
+    Vector2 getWorldPos() { return WorldPos; }
+    Vector2 getScreenPos(){return ScreenPos;}
+    float getFlip(){return Flip;}
+
+    void setScreenPos(float inWidth, float inHeight);
+    void setFlip(float inFlip){this->Flip = inFlip;}
+    void setUpdateTime(float inUpdateTime){ this->UpdateTime = inUpdateTime;}
+    void setMaxFrame(int inMaxFrame){this->MaxFrame = inMaxFrame;}
+    void tick(float deltaTime);
+
+};
 
 // --------------- Forward Declarations ----------------
 Vector2 FindDirection(Vector2 Position, Vector2 WindowSettings, int Scale, float Speed);
-int AnimateCharacter(int Frame, int MaxFrame, float &RunningTime, float UpdateTime, float deltatime);
+int FindAnimationFrame(int Frame, int MaxFrame, float &RunningTime, float UpdateTime, float deltatime);
 
 // =================== Main function ========================
 int main()
 {
-    
     InitWindow(Win_Settings.x, Win_Settings.y, "Game Window");
 
     // ---------------------- general settings ----------------------
-    int Speed{10};
+    int Speed{5};
 
     // Map Settings
-    MapSettings Map{
-        LoadTexture("nature_tileset/Game_Map.png"),
-        {0, 0},
-        0,
-        4,
-        WHITE};
-    // ------------------------ Character Settings ----------------------------------
-    Texture2D Knight_Run = LoadTexture("characters/knight_run_spritesheet.png");
-    Texture2D Knight_Idle = LoadTexture("characters/knight_idle_spritesheet.png");
-    Vector2 knightPos{
-        (float)Win_Settings.x / 2.0f - 4.0f * (0.5f * (float)Knight_Idle.width / 6.0f),
-        (float)Win_Settings.y / 2.0f - 4.0f * (0.5f * (float)Knight_Idle.height)};
+    Texture2D Map = LoadTexture("nature_tileset/Game_Map.png");
+    int MapScale = 4.0f;
+    Vector2 MapPosition{0.0f,0.0f};
+     // ------------------------- Animation Settings ----------------------------------
+    int Frame = 0;
+    int MaxFrame = 6;
+    float RunningTime = 0;
+    float UpdateTime = 12;
 
-    Character KnightObject;
-    KnightObject.setTexture(Knight_Idle);
-    KnightObject.setScreenPosition(knightPos);
-    
+    // ====== Character instance =======
+    Character Knight;
+    Knight.Idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Knight.Run = LoadTexture("characters/knight_run_spritesheet.png");
+    Knight.Current = Knight.Idle;
+    Knight.setFlip(1.0f);
+    Knight.setUpdateTime(UpdateTime);
+    Knight.setMaxFrame(MaxFrame);
+    // ======================================
+    Vector2 Screen_Pos{
+        (float)Win_Settings.x / 2.0f - 4.0f * (0.5f * (float)Knight.Idle.width / 6.0f),
+        (float)Win_Settings.y / 2.0f - 4.0f * (0.5f * (float)Knight.Idle.height)
+        };
+    Knight.setScreenPos(Screen_Pos.x,Screen_Pos.y);
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
-
-        Vector2 FinalDirection = FindDirection(KnightObject.getScreenPosition(), Win_Settings, Map.Scale, Speed);
-        if (Vector2Length(FinalDirection) != 0.0f)
-        {
-            Map.Position = Vector2Subtract(Map.Position, FinalDirection);
-            // FinalDirection.x < 0.0f ? rightleft = -1.0f : rightleft = 1.0f;
-            FinalDirection.x < 0.0f ? Flip = -1.0f : Flip = 1.0f;
-            Current_Knight = Knight_Run;
-        }
-        else
-        {
-            Current_Knight = Knight_Idle;
-        }
-        // TICK FUNCTION CALL
-        KnightObject.Tick(GetFrameTime());
         // ------- drawing map image -------
-        DrawTextureEx(Map.Texture, Map.Position, Map.Rotation, Map.Scale, Map.Tint);
+        MapPosition = Knight.getWorldPos();
+        DrawTextureEx(Map, MapPosition, 0, MapScale, WHITE);
 
         // ------- Drawing the character ----------
-        CharacterAnimation.Frame = AnimateCharacter(CharacterAnimation.Frame,
-                                                    CharacterAnimation.MaxFrame,
-                                                    CharacterAnimation.RunningTime,
-                                                    CharacterAnimation.UpdateTime,
-                                                    GetFrameTime());
-
-        Rectangle Source{
-            CharacterAnimation.Frame * (float)Knight_Idle.width / CharacterAnimation.MaxFrame,
-            0.0f,
-            KnightObject.XFlip * (float)(Knight_Idle.width / 6.0f),
-            (float)Current_Knight.height};
-        Rectangle Dest{
-            knightPos.x, knightPos.y,
-            4.0f * Current_Knight.width / 6.0f,
-            4.0f * Current_Knight.height};
-
-        DrawTexturePro(Current_Knight, Source, Dest, Vector2{}, 0.0f, WHITE);
+        Knight.tick(GetFrameTime());
 
         EndDrawing();
     }
-    UnloadTexture(Map.Texture);
-    UnloadTexture(Current_Knight);
-    UnloadTexture(Knight_Idle);
-    UnloadTexture(Knight_Run);
+    UnloadTexture(Map);
+    UnloadTexture(Knight.Idle);
+    UnloadTexture(Knight.Run);
+    UnloadTexture(Knight.Current);
     CloseWindow();
 }
 
@@ -120,7 +125,7 @@ Vector2 FindDirection(Vector2 Position, Vector2 WindowSettings, int Scale, float
     return Direction;
 }
 
-int AnimateCharacter(int Frame, int MaxFrame, float &RunningTime, float UpdateTime, float deltatime)
+int FindAnimationFrame(int Frame, int MaxFrame, float &RunningTime, float UpdateTime, float deltatime)
 {
 
     RunningTime += deltatime;
@@ -136,107 +141,46 @@ int AnimateCharacter(int Frame, int MaxFrame, float &RunningTime, float UpdateTi
     return Frame;
 }
 
-class Character
+void Character::setScreenPos(float winWidth, float winHeight)
 {
-private:
-    Texture2D Texture;
-    Texture2D Idle;
-    Texture2D Run;
-    Vector2 ScreenPosition;
-    Vector2 WorldPosition;
-    // Character flip
-    float XFlip = 1.0f;
-    float RunningTime = 0.0f;
-    const float UpdateTime = 0.0f;
-    int Frame = 0;
-    const int MaxFrame = 0;
-    float Speed = 5.0f;
+    ScreenPos = {(float)winWidth / 2.0f - 4.0f * (0.5f * (float)Current.width / 6.0f),
+                 (float)winHeight / 2.0f - 4.0f * (0.5f * (float)Current.height)
+    };
+}
 
-public:
-    // ---------------------- Constructor ----------------------
-    Character()
-    {
-        this->ScreenPosition = Vector2{};
-        this->WorldPosition = Vector2{};
-        this->MaxFrame = 0;
-        this->UpdateTime = 0;
-    }
-    Character(const char *FilePath, Vector2 ScreenPos, Vector2 WorldPos, int MaxFrame, float UpdateTime)
-    {
-        this->Texture = LoadTexture(FilePath);
-        this->ScreenPosition = ScreenPos;
-        this->WorldPosition = WorldPos;
-        this->MaxFrame = MaxFrame;
-        this->UpdateTime = UpdateTime;
-    }
-    Character(Texture2D Texture, Vector2 ScreenPos, Vector2 WorldPos)
-    {
-        this->Texture = Texture;
-        this->ScreenPosition = ScreenPos;
-        this->WorldPosition = WorldPos;
-    }
-    // ---------------------- Setters ----------------------
-    void setTexture(Texture2D inTexture)
-    {
-        this->Texture = inTexture;
-    }
-    void setScreenPosition(Vector2 inScreenPos)
-    {
-        this->ScreenPosition = inScreenPos;
-    }
-    void setWorldPosition(Vector2 inWorldPos)
-    {
-        this->WorldPosition = inWorldPos;
-    }
-    void setUpdateTime(float inUpdateTime)
-    {
-        this->UpdateTime = inUpdateTime;
-    }
-    void setMaxFrame(float inMaxFrame)
-    {
-        this->MaxFrame = inMaxFrame;
-    }
-    // ---------------------- Getters ----------------------
-    Texture2D getTexture()
-    {
-        return this->Texture;
-    }
-    Vector2 getScreenPosition()
-    {
-        return this->ScreenPosition;
-    }
-    Vector2 getWorldPosition()
-    {
-        return this->WorldPosition;
-    }
-    float getRunningTime()
-    {
-        return this->RunningTime;
-    }
-    int getFrame()
-    {
-        return this->Frame;
-    }
-    // ---------------------- TickFunction ----------------------
-    void Tick(float DeltaTime);
-};
+void Character::tick(float deltaTime){
 
-void Character::Tick(float deltatime)
-{
-    Vector2 FinalDirection = FindDirection(ScreenPosition, Win_Settings, Map.Scale, Speed);
+
+    Vector2 FinalDirection = FindDirection(WorldPos,Win_Settings,4.0,5.0);
     if (Vector2Length(FinalDirection) != 0.0f)
     {
-        Map.Position = Vector2Subtract(Map.Position, FinalDirection);
-        // FinalDirection.x < 0.0f ? rightleft = -1.0f : rightleft = 1.0f;
-        FinalDirection.x < 0.0f ? Character.Flip = -1.0f : Character.Flip = 1.0f;
-        Current_Knight = Knight_Run;
+            WorldPos = Vector2Add(WorldPos, FinalDirection);
+            // FinalDirection.x < 0.0f ? rightleft = -1.0f : rightleft = 1.0f;
+            WorldPos.x < 0.0f ? Flip = -1.0f : Flip = 1.0f;
+            Current = Run;
     }
     else
     {
-        Current_Knight = Knight_Idle;
+            Current = Idle;
     }
+
+    Frame = FindAnimationFrame(Frame,MaxFrame,RunningTime,UpdateTime,deltaTime);
+     Rectangle Source{
+            Frame * (float)Current.width / MaxFrame,
+            0.0f,
+            getFlip() * (float)(Current.width / 6.0f),
+            (float)Current.height};
+        Rectangle Dest{
+            getScreenPos().x, getScreenPos().y,
+            4.0f * Current.width / 6.0f,
+            4.0f * Current.height};
+
+        DrawTexturePro(Current, Source, Dest, Vector2{}, 0.0f, WHITE);
 }
 
-class Map
-{
-};
+
+
+
+
+
+
